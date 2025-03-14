f677d22108f9a2b201cbae99669bf7a974c694bc
Leonardo <leogusmaocf@gmail.com>
2025-03-14 16:46:28 -0300

    chore: add AWS-S3 service to store images

--- app/meals/[slug]/page.jsx
+++ app/meals/[slug]/page.jsx
@@ -18,7 +18,7 @@ async function Meal({ slug }) {
     <>
       <header className={classes.header}>
         <div className={classes.image}>
-          <Image src={meal.image} alt={meal.title} fill />
+          <Image src={`https://your-s3-bucket.amazonaws.com/${meal.image}`} alt={meal.title} fill />
         </div>
         <div className={classes.headerText}>
           <h1>{meal.title}</h1>
